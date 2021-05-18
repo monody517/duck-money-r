24d4402ad0102dcf0457ad920c68a6e0d3e0d294
Monody517 <2737716714@qq.com>
2021-05-18 15:54:09 +0800

    number功能基本完成

--- src/components/money/NumberSection.tsx
+++ src/components/money/NumberSection.tsx
@@ -8,7 +8,7 @@ $bg:#fbaa00;
         font-family: Consolas, monospace;
         padding: 5px 16px;
         text-align: right;
-        border:1px solid #d9d9d9
+        border:1px solid #d9d9d9;
     }
     .buttons{
         > button{
@@ -35,7 +35,15 @@ $bg:#fbaa00;
 `
 
 const NumberSection:FC = () => {
-    const [output,setOutput] = useState('0')
+    const [output,_setOutput] = useState('0')
+    const setOutput = (output:string) => {
+        if(output.length>=16){
+            return
+        }else if(output.length === 0){
+            output = '0'  
+        }
+        _setOutput(output)
+    }
     const onClickButtons = (e:React.MouseEvent) => {
         const text = (e.target as HTMLButtonElement).textContent
         if(text === null){return}
@@ -50,23 +58,28 @@ const NumberSection:FC = () => {
             case '8':
             case '9':
             case '0':
-            case '.':
-                if(output === '0'){
+                if(output === '0'){               
                     setOutput(text)
                 }else{
                     setOutput(output+text)
                 }
                 break;
             case '删除':
-                console.log('删除');
+                setOutput(output.slice(0,-1))
                 break;
             case '清空':
-                console.log('清空');
+                setOutput('0')
                 break;
             case 'OK':
                 console.log('ok');
-                break; 
-        }    
+                break;
+            case '.':
+                if(output.indexOf('.')>=0){
+                    return
+                }else{
+                    setOutput(output+'.')
+                }
+        }
     }
     return(
         <Wrapper>
