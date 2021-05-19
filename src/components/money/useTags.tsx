e58488d38c3b637473f996958eaf5a93c7d6777a
Monody517 <2737716714@qq.com>
2021-05-19 12:06:13 +0800

    Tags样式

--- src/components/money/useTags.tsx
+++ src/components/money/useTags.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const useTags= () => {
-    const [tags,setTags] = useState<string[]>(['衣','食','住','行'])
+    const [tags,setTags] = useState<string[]>(['衣','食','住','行','学'])
     return(
         {tags,
         setTags}
--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,16 +1,68 @@
 import Layout from "components/Layout";
 import { useTags } from "components/money/useTags";
 import React from "react";
+import styled from "styled-components";
+import Icon from '../components/icon'
+
+const Wrapper = styled.section`
+  display:flex;
+  flex-grow:1;
+  flex-direction:column;
+    > ol{
+        display:flex;
+        flex-wrap: wrap;
+        margin-left: 23px;
+        overflow: auto;
+        align-content: flex-start;
+        flex-grow:1;
+        > li{
+            display: flex;
+            padding: 2px 20px;
+            border-radius: 12px;
+            margin-right: 20px;
+            margin-top: 10px;
+            flex-direction: column-reverse;
+            justify-content: center;
+            align-items: center;
+            border: 1px solid #c9c9c9;
+            .icon{
+                width: 50px;
+                height: 25px;   
+                }  
+        }
+    }
+    .createdTag{
+        display: flex;
+        background: #fbaa00;
+        padding: 15px 0;
+        span{
+            color: white;
+            position: relative;
+            left: 50%;
+            margin-left: -32px;
+        }
+    }
+`
 
 function Tags() {
   const {tags,setTags} = useTags()
     return (
       <Layout>
+        <Wrapper>
         <ol>
           {tags.map(t=>{
-            return <li key='t'>{t}</li>
+            return(
+              <li key={t}>
+                {t}
+                <Icon name={t}></Icon> 
+              </li>
+            )
           })}
         </ol>
+        <div className='createdTag'>
+          <span>新建标签</span>
+        </div>
+        </Wrapper>
       </Layout>
       );
   }
