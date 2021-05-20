ffe1c963958c2f0f20c72508b325c33c4fc70019
Monody517 <2737716714@qq.com>
2021-05-20 10:38:11 +0800

    icon加星

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
 try {importAll(require.context('../icons', true, /\.svg$/));} catch (error) {console.log(error);}
 
@@ -8,9 +9,17 @@ type Prop = {
 }
 
 const icon = (prop:Prop) => {
+    const existTag = ['食物','房租','购物','水果','家居','学习','工资','兼职','礼金','投资','left','money','statistics','tag','标签管理']
+    const findName = (name:string) => {
+        if(existTag.indexOf(name)>=0){
+            return name
+        }else{
+            return 'star'
+        }
+    }
     return(
         <svg className='icon'>
-            <use xlinkHref={'#' + prop.name}/>
+            <use xlinkHref={'#' +findName(prop.name)}/>
         </svg>
     )
 }
--- src/components/money/TagsSection.tsx
+++ src/components/money/TagsSection.tsx
@@ -1,4 +1,3 @@
-import { createId } from 'lib/createId';
 import React, { FC } from 'react'
 import { Link } from 'react-router-dom';
 import styled from "styled-components";
